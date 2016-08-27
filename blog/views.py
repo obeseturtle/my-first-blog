c495d65eef4618eeecf123fcd22e71e0a1ea5cf4
obeseturtle <obeseturtle@gmail.com>
2016-08-27 15:27:58 +0800

    new

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
-#views connect models and templates
-from .models import Post
-#. means current directory
-#post - name of model
 from django.utils import timezone
+from .models import Post
+
 def post_list(request):
-#need to take models to display, pass them to template
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order)by('published_date')
-#render - request =, template file, {} template use	
